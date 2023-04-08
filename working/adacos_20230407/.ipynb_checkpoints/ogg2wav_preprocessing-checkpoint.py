dc4cc8eb7f1dd2e12ef588ec444755ceb871ca26
HirokiNarita <hirokin1999@outlook.jp>
2023-04-08 14:14:04 +0000

    trainingを追加

---
+++ working/adacos_20230407/.ipynb_checkpoints/ogg2wav_preprocessing-checkpoint.py
@@ -0,0 +1,193 @@
+# implements by https://www.kaggle.com/code/nischaydnk/birdclef-2023-pytorch-lightning-inference
+import copy
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa as lb
+import soundfile as sf
+
+import torch
+import torchaudio
+from torch.utils.data import Dataset
+
+from config import CFG
+
+def compute_melspec(y, sr, n_fft, hop_length, n_mels, fmin, fmax, power=2.0):
+    """
+    Computes a mel-spectrogram and puts it at decibel scale
+    Arguments:
+        y {np array} -- signal
+        params {AudioParams} -- Parameters to use for the spectrogram. Expected to have the attributes sr, n_mels, f_min, f_max
+    Returns:
+        np array -- Mel-spectrogram
+    """
+    mel_spectrogram = lb.feature.melspectrogram(
+            y=y,
+            sr=sr,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            n_mels=n_mels,
+            fmin=fmin,
+            fmax=fmax,
+            power=power,
+        )
+    # convert melspectrogram to log mel energies
+    melspec = (
+        20.0 / power * np.log10(np.maximum(mel_spectrogram, sys.float_info.epsilon))
+    )
+    return melspec
+
+def mono_to_color(X, eps=1e-6, mean=None, std=None):
+    mean = mean or X.mean()
+    std = std or X.std()
+    X = (X - mean) / (std + eps)
+    
+    _min, _max = X.min(), X.max()
+
+    if (_max - _min) > eps:
+        V = np.clip(X, _min, _max)
+        V = 255 * (V - _min) / (_max - _min)
+        V = V.astype(np.uint8)
+    else:
+        V = np.zeros_like(X, dtype=np.uint8)
+
+    return V
+
+def crop_or_pad(y, length, is_train=True, start=None):
+    if len(y) < length:
+        y = np.concatenate([y, np.zeros(length - len(y))])
+        
+        n_repeats = length // len(y)
+        epsilon = length % len(y)
+        
+        y = np.concatenate([y]*n_repeats + [y[:epsilon]])
+        
+    elif len(y) > length:
+        if not is_train:
+            start = start or 0
+        else:
+            start = start or np.random.randint(len(y) - length)
+
+        y = y[start:start + length]
+    
+    else:
+        return y
+
+    return y
+
+def crop_or_pad(y, length, is_train=True, start=None):
+    if len(y) < length:
+        y = torch.cat([y, torch.zeros(length - len(y))])
+        
+        n_repeats = length // len(y)
+        epsilon = length % len(y)
+        
+        y = torch.cat([y]*n_repeats + [y[:epsilon]])
+        
+    elif len(y) > length:
+        if not is_train:
+            start = start or 0
+        else:
+            start = start or np.random.randint(len(y) - length)
+
+        y = y[start:start + length]
+    
+    else:
+        return y
+
+    return y
+
+class BirdDataset(Dataset):
+    def __init__(self,
+                 data,
+                 sr=CFG.sample_rate,
+                 n_fft=CFG.n_fft,
+                 n_mels=CFG.n_mels,
+                 hop_length=CFG.hop_length,
+                 fmin=0,
+                 fmax=None,
+                 duration=CFG.duration,
+                 step=None,
+                 is_train=True):
+        
+        self.data = data
+        
+        self.sr = sr
+        
+        self.n_fft = n_fft
+        self.n_mels = n_mels
+        self.fmin = fmin
+        self.fmax = fmax or self.sr//2
+        self.hop_length = hop_length
+
+        self.duration = duration
+        self.audio_length = self.duration*self.sr
+        self.is_train = is_train
+        self.step = step or self.audio_length
+
+    def __len__(self):
+        return len(self.data)
+    
+    @staticmethod
+    def normalize(image):
+        image = image.astype("float32", copy=False) / 255.0
+        return image
+    
+    def audio_to_image(self, audio):
+        image = compute_melspec(audio, self.sr, self.n_fft, self.hop_length, self.n_mels, self.fmin, self.fmax)
+        #image = mono_to_color(melspec)
+        #image = self.normalize(image)
+        return image
+
+    def read_file(self, filepath):
+        audio, sr = lb.load(filepath, sr=self.sr, mono=True)
+        #audio, sr = torchaudio.load(filepath)
+        # # train
+        # if self.is_train == True:
+        #     # adjust audio length
+        #     audio = crop_or_pad(audio, length=self.audio_length, is_train=self.is_train)
+        #     #images = self.audio_to_image(audio)
+        #     #images = torch.from_numpy(images)
+        # # inference
+        # else:
+        #     images = []
+        #     # make subseq
+        #     for idx, i in enumerate(range(0, len(audio), self.audio_length)):
+        #         # index
+        #         start = i
+        #         end = start + self.audio_length
+        #         # crop
+        #         audio_ = audio[start:end]
+        #         # 長さが5秒に満たない場合は切り捨て
+        #         if (len(audio_) < self.audio_length) and (idx != 0):
+        #             continue
+        #         audio_ = crop_or_pad(audio_, length=self.audio_length, is_train=self.is_train)
+        #         # to spectrogram
+        #         image = self.audio_to_image(audio_)
+        #         image = torch.from_numpy(image)
+                
+        #         images.append(image)
+        #     images = torch.stack(images, dim=0)
+        
+        return audio
+        
+    def __getitem__(self, idx):
+        sample_info = self.data.loc[idx]
+        audio, sr = lb.load(sample_info['filepath'], sr=self.sr, mono=True)
+        filename = sample_info['filename']
+        filename = filename.replace('.ogg', '.wav')
+        out_path = CFG.BASE_PATH + '/train_audio_wav/' + filename
+        sample_info = sample_info.to_dict()
+        # save
+        sf.write(out_path, audio, sr, format='wav', subtype='FLOAT')
+        
+        
+        # if self.is_train == False:
+        #     sample_info_tmp = []
+        #     for i in range(len(features)):
+        #         sample_info_ = copy.deepcopy(sample_info)
+        #         sample_info_tmp.append(sample_info_)
+        #     sample_info = sample_info_tmp
+            
+        return audio, sample_info
---
+++ working/adacos_20230407/pytorch_model.py
@@ -0,0 +1,143 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.nn import Parameter
+import math
+
+import timm
+import torchaudio
+from torchaudio.transforms import MelSpectrogram
+from torchaudio.transforms import AmplitudeToDB
+from torchlibrosa.augmentation import DropStripes
+
+from config import CFG
+
+class AdaCos(nn.Module):
+    def __init__(self, num_features, num_classes, m=0.50):
+        super(AdaCos, self).__init__()
+        self.num_features = num_features
+        self.n_classes = num_classes
+        self.s = math.sqrt(2) * math.log(num_classes - 1)
+        self.m = m
+        self.W = Parameter(torch.FloatTensor(num_classes, num_features))
+        nn.init.xavier_uniform_(self.W)
+
+    def forward(self, input, label=None):
+        # normalize features
+        x = F.normalize(input)
+        # normalize weights
+        W = F.normalize(self.W)
+        # dot product
+        logits = F.linear(x, W)
+        if label is None:
+            return logits
+        # feature re-scale
+        theta = torch.acos(torch.clamp(logits, -1.0 + 1e-7, 1.0 - 1e-7))
+        target_logits = torch.cos(theta + self.m)
+        one_hot = label
+        output = logits * (1 - one_hot) + target_logits * one_hot
+        if self.training:
+            with torch.no_grad():
+                B_avg = torch.where(one_hot < 1, self.s * torch.exp(logits), torch.zeros_like(logits))
+                B_avg = torch.sum(B_avg) / input.size(0)
+                # print(B_avg)
+                theta_med = torch.median(theta)
+                self.s = torch.log(B_avg) / torch.cos(torch.min(math.pi/4 * torch.ones_like(theta_med), theta_med))
+        output *= self.s
+
+        return output
+
+class BirdCLEF23Net(nn.Module):
+    def __init__(self,
+                 num_classes=CFG.num_classes,
+                 model_name=CFG.model_name,
+                 in_chans=CFG.in_chans,
+                 sample_rate=CFG.sample_rate,
+                 n_fft=CFG.n_fft,
+                 hop_length=CFG.hop_length,
+                 n_mels=CFG.n_mels,
+                 top_db=CFG.top_db):
+       
+        super(BirdCLEF23Net, self).__init__()
+        self.model=timm.create_model(pretrained=True,
+                                     model_name=model_name,
+                                     in_chans=in_chans,
+                                     num_classes=num_classes,)
+        self.sr=sample_rate
+        self.n_fft=n_fft
+        self.hop_length=hop_length
+        self.n_mels=n_mels
+        self.num_classes=num_classes
+        self.adacos = AdaCos(num_features=1280, num_classes=num_classes)
+        
+        self.to_melspec_fn = MelSpectrogram(sample_rate=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+        self.to_log_scale_fn = AmplitudeToDB(top_db=top_db)
+        
+        self.freq_dropper = DropStripes(dim=2, drop_width=int(CFG.freq_mask/2), 
+            stripes_num=2)
+        self.time_dropper = DropStripes(dim=3, drop_width=int(CFG.time_mask/2), 
+            stripes_num=2)
+        
+    
+    def wav_to_logmel(self, x):
+        # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+        x = self.to_melspec_fn(x)
+        # Convert to decibels
+        x = self.to_log_scale_fn(x)
+        return x
+    
+    def mixup(self, data, label, alpha=0.5, device='cuda:0'):
+        def _aug(data1, data2, weights):
+            batch_size = len(data1)
+            mix_data = []
+            for i in range(batch_size):
+                sample = data1[i]*weights[i] + data2[i]*(1 - weights[i])
+                mix_data.append(sample)
+            
+            mix_data = torch.stack(mix_data)
+
+            return mix_data
+        
+        batch_size = len(data)
+        weights = torch.from_numpy(np.random.uniform(low=0, high=alpha, size=batch_size)).to(device)
+        index = np.random.permutation(batch_size)
+        x1, x2 = data, data[index]
+        y1, y2 = label, label[index]
+
+        x = _aug(x1, x2, weights)
+        y = _aug(y1, y2, weights)
+
+        return x, y
+    
+    def forward(self, wav, targets=None):
+        # wav has shape [channel, time]
+        wav = wav.unsqueeze(1)
+        # wav augment
+        #if self.training == True:
+        #    wav = 
+        x = self.wav_to_logmel(wav)
+        #plt.imshow(x[0,0,:,:].to('cpu'))
+        #plt.show()
+        # log-mel augment
+        if self.training == True:
+            # TODO mixup
+            x, y = self.mixup(x, targets)
+            # TODO specaug
+            x = self.time_dropper(self.freq_dropper(x))
+        else:
+            y = targets
+        # plt.imshow(x[0,0,:,:].to('cpu'))
+        # plt.show()
+        # feature extractor
+        x = self.model.forward_features(x)
+        x = self.model.global_pool(x)
+        #x = self.model.forward_head(x, pre_logits=False)
+        x = self.adacos(x, y)
+        #x = self.model(x)
+        if self.training == True:
+            return x, y
+        else:
+            return x
